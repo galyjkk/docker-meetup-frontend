41474b42953308c5f5bccd08c5c8443eed4ec7ab
galyjkk <galyjkk@126.com>
2014-07-10 15:22:35 +0800

    updates

--- app/scripts/controllers/base.js
+++ app/scripts/controllers/base.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dockerMeetupFrontendApp')
-  .controller('HomeBaseCtrl', function ($scope) {
+  .controller('HomeBaseCtrl', function ($scope, $http) {
     $scope.selected = undefined;
     $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
@@ -28,5 +28,21 @@ angular.module('dockerMeetupFrontendApp')
 
     $scope.addLibrary = function(item) {
       console.log(item);
+    };
+
+    $scope.getImage = function(val) {
+      return $http.get('https://index.docker.io/v1/search?q='+val, {
+        headers: {
+          'Access-Control-Allow-Headers': 'index.docker.io',
+          'Accept': 'application/json'
+        }
+      }).then(function(res){
+        console.log(res);
+        var addresses = [];
+        // angular.forEach(res.data.results, function(item){
+        //   addresses.push(item.formatted_address);
+        // });
+        return addresses;
+      });
     };
   });
